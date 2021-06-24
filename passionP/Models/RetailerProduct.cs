d2ec2771346c4349064e4b940abc87913ae9beb6
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-06-24 14:03:34 -0400

    Add files via upload

---
+++ passionP/Models/RetailerProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace passionP.Models
+{
+    public class RetailerProduct
+    {
+        public int Price { get; set; }
+
+        [Key, Column(Order = 2), ForeignKey("Product")]
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Key, Column(Order = 1), ForeignKey("Retailer")]
+        public int RetailerID { get; set; }
+        public virtual Retailer Retailer { get; set; }
+    }
+}
